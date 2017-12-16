55cc293eb00b9b90d961702b9f8c0a4c4be83cb2
Ciro S. Costa <ciro.costa@usp.br>
2017-12-16 16:21:46 +0000

    Implements first working logging flow

--- driver/driver.go
+++ driver/driver.go
@@ -2,13 +2,17 @@ package driver
 
 import (
 	"context"
+	"encoding/binary"
 	"io"
+
 	"os"
 	"path"
 	"sync"
 	"syscall"
 
 	"github.com/cirocosta/oklog-docker-plugin/docker"
+	"github.com/docker/docker/api/types/plugins/logdriver"
+	protoio "github.com/gogo/protobuf/io"
 	"github.com/tonistiigi/fifo"
 
 	"github.com/pkg/errors"
@@ -25,10 +29,13 @@ type Driver struct {
 type logPair struct {
 	stream io.ReadCloser
 	info   docker.Info
+	active bool
 }
 
 func New() (d Driver) {
 	d.logger = zerolog.New(os.Stdout)
+	d.logs = make(map[string]*logPair)
+
 	return
 }
 
@@ -54,18 +61,105 @@ func (d *Driver) StartLogging(file string, info docker.Info) (err error) {
 		return errors.Wrapf(err, "error opening logger fifo: %q", file)
 	}
 
-	d.logs[name]  = &logPair{
-		info: info,
+	lp := &logPair{
+		info:   info,
 		stream: stream,
+		active: true,
 	}
+	d.logs[name] = lp
+
+	go d.ConsumeLog(lp)
 
 	return
+}
+
+func (d *Driver) ConsumeLog(lp *logPair) {
+	var (
+		buf logdriver.LogEntry
+		dec = protoio.NewUint32DelimitedReader(
+			lp.stream, binary.BigEndian, 1e6)
+	)
+
+	defer dec.Close()
+	defer d.ShutdownLogPair(lp)
+
+	for {
+		if !lp.active {
+			d.logger.Debug().
+				Str("id", lp.info.ContainerID).
+				Msg("shutting down logger goroutine due to stop request")
+			return
+		}
+
+		err := dec.ReadMsg(&buf)
+		if err != nil {
+			if err == io.EOF {
+				d.logger.Debug().
+					Err(err).
+					Str("id", lp.info.ContainerID).
+					Msg("shutting down logger goroutine due to file EOF")
+				return
+			}
+
+			d.logger.Warn().
+				Err(err).
+				Str("id", lp.info.ContainerID).
+				Msg("error reading from FIFO, trying to continue")
+
+			dec = protoio.NewUint32DelimitedReader(
+				lp.stream, binary.BigEndian, 1e6)
+			continue
+		}
+
+		err = d.DoSomethingWithLog(lp, buf.Line)
+		if err != nil {
+			d.logger.Warn().
+				Str("id", lp.info.ContainerID).
+				Err(err).
+				Msg("error logging message, dropping it and continuing")
+		}
+
+		buf.Reset()
+	}
+}
+
+func (d *Driver) DoSomethingWithLog(lp *logPair, line []byte) (err error) {
+	d.logger.Info().
+		Str("CONTAINER", lp.info.ContainerID).
+		Msg(string(line))
+
+	return
+}
+
+func (d *Driver) ShutdownLogPair(lp *logPair) {
+	if lp.stream != nil {
+		lp.stream.Close()
+	}
+
+	lp.active = false
 }
 
 func (d *Driver) StopLogging(file string) (err error) {
 	d.logger.Info().
 		Str("file", file).
 		Msg("stop")
+
+	var name = path.Base(file)
+
+	d.mu.Lock()
+	defer d.mu.Unlock()
+
+	lp, ok := d.logs[name]
+
+	if !ok {
+		d.logger.Warn().
+			Str("file", file).
+			Msg("failed to stop logging - file not active")
+		return
+	}
+
+	lp.active = false
+	delete(d.logs, path.Base(file))
 
 	return
 }
